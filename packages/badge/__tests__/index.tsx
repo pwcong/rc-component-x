34a8d17c0d702e84ec05bb2957f7e52cd4b4ef39
pwcong <pwcong@foxmail.com>
2019-05-14 22:15:03 +0800

    feat: 新增badge

--- packages/badge/__tests__/index.tsx
+++ packages/badge/__tests__/index.tsx
@@ -8,8 +8,74 @@ export default function() {
   return (
     <Test
       component={Badge}
-      componentProps={{}}
-      componentTestProps={{}}
+      componentProps={{
+        children: (
+          <div
+            style={{
+              display: 'inline-block',
+              border: '1px solid #cccccc',
+              padding: '48px'
+            }}
+          />
+        )
+      }}
+      componentTestProps={{
+        color: {
+          type: 'input',
+          label: '颜色',
+          value: ''
+        },
+        title: {
+          type: 'input',
+          label: '标题',
+          value: '999+'
+        },
+        dot: {
+          type: 'switch',
+          label: '只显示点',
+          value: false
+        },
+        size: {
+          type: 'radio',
+          label: '尺寸',
+          value: 'default',
+          options: [
+            { label: 'default', value: 'default' },
+            { label: 'large', value: 'large' },
+            { label: 'small', value: 'small' }
+          ]
+        },
+        shape: {
+          type: 'radio',
+          label: '形状',
+          value: 'round',
+          options: [
+            { label: 'round', value: 'round' },
+            { label: 'square', value: 'square' }
+          ]
+        },
+        type: {
+          type: 'radio',
+          label: '类型',
+          value: 'default',
+          options: [
+            { label: 'default', value: 'default' },
+            { label: 'primary', value: 'primary' },
+            { label: 'success', value: 'success' },
+            { label: 'warning', value: 'warning' },
+            { label: 'error', value: 'error' }
+          ]
+        },
+        offset: {
+          type: 'radio',
+          label: '位置偏移',
+          value: undefined,
+          options: [
+            { label: 'undefined', value: 'undefined' },
+            { label: 'x: 8, y: 8', value: { x: 8, y: 8 } }
+          ]
+        }
+      }}
       componentPackageJson={packageJson}
     />
   );
--- packages/badge/index.tsx
+++ packages/badge/index.tsx
@@ -15,6 +15,8 @@ export type IBadgeType =
 
 export type IBadgeSize = 'default' | 'large' | 'small';
 
+export type IBadgeShape = 'square' | 'round';
+
 export interface IBadgeProps {
   /** 类名 */
   className?: string;
@@ -22,19 +24,74 @@ export interface IBadgeProps {
   title?: string;
   /** 类型 */
   type?: IBadgeType;
+  /** 只显示点 */
+  dot?: boolean;
   /** 尺寸 */
   size?: IBadgeSize;
   /** 颜色，若设置颜色则 type 属性失效 */
   color?: string;
   /** 位置偏移 */
   offset?: {
-    x: number;
-    y: number;
+    x?: number;
+    y?: number;
   };
+  /** 形状 */
+  shape?: IBadgeShape;
+  /** 子内容 */
+  children?: React.ReactNode;
 }
 
-const Badge = (props: IBadgeProps) => {
-  return <div className={classNames(baseCls)}>Hello World!</div>;
+const Badge: React.FunctionComponent<IBadgeProps> = props => {
+  const {
+    className,
+    title,
+    type = 'default',
+    size = 'default',
+    shape = 'round',
+    dot,
+    color,
+    offset = {},
+    children
+  } = props;
+
+  const { x = 0, y = 0 } = offset;
+
+  const badge = (
+    <div
+      className={classNames(baseCls, className, {
+        [`${getPrefixCls(size, baseCls)}`]: size,
+        [`${getPrefixCls(type, baseCls)}`]: type,
+        [`${getPrefixCls(shape, baseCls)}`]: shape,
+        [`${getPrefixCls('dot', baseCls)}`]: dot
+      })}
+      title={title}
+      style={{
+        backgroundColor: color,
+        top: y,
+        left: x
+      }}
+    >
+      <span>{title}</span>
+    </div>
+  );
+
+  if (children !== undefined) {
+    return (
+      <div className={getPrefixCls('wrapper', baseCls)}>
+        <div className={getPrefixCls('inner', baseCls)}>{badge}</div>
+        {children}
+      </div>
+    );
+  }
+
+  return badge;
+};
+
+Badge.defaultProps = {
+  size: 'default',
+  type: 'default',
+  shape: 'round',
+  offset: {}
 };
 
 export default Badge;
