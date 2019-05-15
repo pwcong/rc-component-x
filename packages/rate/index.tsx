1b711a1dc9db72b64cdf8a530cc505f64036aa67
彭伟聪 <pengwc@landray.com.cn>
2019-05-15 18:05:48 +0800

    update

--- packages/rate/index.tsx
+++ packages/rate/index.tsx
@@ -6,10 +6,38 @@ import './style.scss';
 
 const baseCls = getPrefixCls('rate');
 
-export interface IProps {}
+export interface IRateProps {
+  className?: string;
+  allowClear?: boolean;
+  allowHalf?: boolean;
+  character?: string;
+  count?: number;
+  value?: number;
+  defaultValue?: number;
+  disabled?: boolean;
+  style?: React.CSSProperties;
+  onChange?: (value: number) => void;
+}
 
-const Rate = (props: IProps) => {
-  return <div className={classNames(baseCls)}>Hello World!</div>;
-};
+export interface IState {
+  value: number;
+}
 
-export default Rate;
+export default class Rate extends React.PureComponent<IRateProps, IState> {
+  static defaultProps: IRateProps = {
+    character: '⭐',
+    count: 5
+  };
+
+  constructor(props: IRateProps) {
+    super(props);
+
+    this.state = {
+      value: props.defaultValue !== undefined ? props.defaultValue : 0
+    };
+  }
+
+  render() {
+    return <div className={classNames(baseCls)} />;
+  }
+}
