c4cec6dacf73e865ff0a8f1d25f77ebf0d320013
彭伟聪 <pengwc@landray.com.cn>
2019-05-16 10:34:47 +0800

    feat: add Rate

--- packages/rate/__tests__/index.tsx
+++ packages/rate/__tests__/index.tsx
@@ -9,7 +9,51 @@ export default function() {
     <Test
       component={Rate}
       componentProps={{}}
-      componentTestProps={{}}
+      componentTestProps={{
+        allowClear: {
+          type: 'switch',
+          value: false,
+          label: '允许清除'
+        },
+        allowHalf: {
+          type: 'switch',
+          value: false,
+          label: '允许一半'
+        },
+        character: {
+          type: 'radio',
+          value: undefined,
+          label: '自定义符号',
+          options: [
+            { label: 'undefined', value: undefined },
+            { label: '好', value: '好' }
+          ]
+        },
+        count: {
+          type: 'inputNumber',
+          value: 5,
+          label: '数值范围'
+        },
+        value: {
+          type: 'radio',
+          value: undefined,
+          label: '自定义值',
+          options: [
+            { label: 'undefined', value: undefined },
+            { label: '4', value: 4 }
+          ]
+        },
+        defaultValue: {
+          type: 'inputNumber',
+          value: 2.5,
+          label: '默认数值'
+        },
+        disabled: {
+          type: 'switch',
+          value: false,
+          label: '禁用'
+        }
+      }}
       componentPackageJson={packageJson}
     />
   );
--- packages/rate/index.tsx
+++ packages/rate/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Icon, { IIconProps } from '@rc-x/icon';
 import { classNames, getPrefixCls } from '@rc-x/utils';
 
 import './style.scss';
@@ -10,7 +11,7 @@ export interface IRateProps {
   className?: string;
   allowClear?: boolean;
   allowHalf?: boolean;
-  character?: string;
+  character?: string | React.ReactElement<IIconProps>;
   count?: number;
   value?: number;
   defaultValue?: number;
@@ -21,11 +22,12 @@ export interface IRateProps {
 
 export interface IState {
   value: number;
+  hoverValue?: number;
 }
 
 export default class Rate extends React.PureComponent<IRateProps, IState> {
   static defaultProps: IRateProps = {
-    character: '⭐',
+    character: <Icon type="star" />,
     count: 5
   };
 
@@ -33,11 +35,106 @@ export default class Rate extends React.PureComponent<IRateProps, IState> {
     super(props);
 
     this.state = {
-      value: props.defaultValue !== undefined ? props.defaultValue : 0
+      value: props.defaultValue !== undefined ? props.defaultValue : 0,
+      hoverValue: undefined
     };
   }
 
+  handleChange = (value: number) => {
+    const { disabled, allowClear, onChange } = this.props;
+
+    if (disabled) {
+      return;
+    }
+
+    if (allowClear && value === this.state.value) {
+      this.setState({
+        value: 0
+      });
+      onChange && onChange(0);
+    } else {
+      this.setState({
+        value
+      });
+      onChange && onChange(value);
+    }
+  };
+
   render() {
-    return <div className={classNames(baseCls)} />;
+    const {
+      className,
+      allowHalf,
+      character,
+      count,
+      value: customValue,
+      disabled,
+      style
+    } = this.props;
+
+    const { hoverValue, value: stateValue } = this.state;
+
+    const value =
+      hoverValue !== undefined
+        ? hoverValue
+        : customValue !== undefined
+        ? customValue
+        : stateValue;
+
+    return (
+      <div
+        className={classNames(baseCls, className, {
+          [`${getPrefixCls('disabled', baseCls)}`]: disabled
+        })}
+        onMouseLeave={() => {
+          this.setState({
+            hoverValue: undefined
+          });
+        }}
+        style={style}
+      >
+        {Array.from(new Array(count)).map((_, index) => {
+          const cls = getPrefixCls('star', baseCls);
+
+          return (
+            <div key={`${cls}-${index}`} className={cls}>
+              {allowHalf && (
+                <div
+                  onMouseEnter={() => {
+                    !disabled &&
+                      this.setState({
+                        hoverValue: index + 0.5
+                      });
+                  }}
+                  onClick={() => {
+                    this.handleChange(index + 0.5);
+                  }}
+                  className={classNames(getPrefixCls('first', cls), {
+                    [`${getPrefixCls('active', cls)}`]: index + 0.5 <= value
+                  })}
+                >
+                  {character}
+                </div>
+              )}
+              <div
+                onMouseEnter={() => {
+                  !disabled &&
+                    this.setState({
+                      hoverValue: index + 1
+                    });
+                }}
+                onClick={() => {
+                  this.handleChange(index + 1);
+                }}
+                className={classNames(getPrefixCls('second', cls), {
+                  [`${getPrefixCls('active', cls)}`]: index + 1 <= value
+                })}
+              >
+                {character}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
